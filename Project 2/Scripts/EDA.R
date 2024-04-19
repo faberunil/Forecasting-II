8c219b9e3bbb0d6c3bfd39223afd4c90987c2e4e
faberb <faberb@users.noreply.github.com>
2024-04-19 15:57:43 +0200

    first graph

--- Project 2/Scripts/EDA.R
+++ Project 2/Scripts/EDA.R
@@ -1,8 +1,20 @@
-# Load the here library
+# Load the packages
 library(here)
+library(tsibble)
+library(ggplot2)
 
 # Read the data using a path relative to the project root including the 'Project 2' folder
 data <- read.csv(here("Project 2", "Data", "seatemp.csv"), header = TRUE, skip = 2)
 
-# Check the first few rows to ensure it's loaded correctly
-head(data)
+# Convert the data frame to a tsibble
+ts_data <- data |>
+  transform(Date = as.Date(paste(year, month, day, sep = "-"))) |>
+  as_tsibble(index = Date)
+
+# Plot the temperature data using ggplot2
+ggplot(ts_data, aes(x = Date, y = mean.temperature.deg.C)) +
+  geom_line() +
+  labs(title = "Daily Sea Surface Temperature",
+       x = "Date",
+       y = "Temperature (°C)") +
+  theme_minimal()
