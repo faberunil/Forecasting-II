3ab890eb83675a3d5d1028852f54d9b66198391a
robinm98 <robin.michel@unil.ch>
2024-05-07 14:08:37 +0200

    push

--- Project 2/Scripts/EDA.R
+++ Project 2/Scripts/EDA.R
@@ -5,12 +5,12 @@ library(ggplot2)
 library(fpp3)
 
 
+### Atlantic data ###
+#####################
+
 ### DATA ###
-############
 
-### Atlantic data
-
-# Read the data using a path relative to the project root including the 'Project 2' folder
+# Read the data 
 atl_data <- read.csv(here("Project 2", "Data", "Atlantic sea.csv"), header = TRUE, skip = 6)
 
 # Convert the data frame to a tsibble
@@ -25,10 +25,10 @@ summary(is.na(ts_data$mean.temperature.deg.C)) # <-- no missing values
 summary(ts_data$mean.temperature.deg.C)
 
 
-### Visualisation ###
-#####################
+### VISUAlISATION ###
 
-### Timeseries plot ###
+
+### Timeseries plot
 
 # Plot the temperature data using ggplot2
 ggplot(ts_data, aes(x = Date, y = mean.temperature.deg.C)) +
@@ -41,7 +41,7 @@ ggplot(ts_data, aes(x = Date, y = mean.temperature.deg.C)) +
 # Same with autoplot
 ts_data |> autoplot(mean.temperature.deg.C) + ggtitle("Daily Sea Surface Temperature") + ylab("Temperature (°C)") + xlab("Time")
 
-### Seasonal plot ###
+### Seasonal plot
 
 # Plot the seasonality with gg_season
 ts_data |>
@@ -66,3 +66,64 @@ components(dcmp) %>% autoplot() + xlab("Year")
 # Plot season adjusted
 ts_data |>
   autoplot(mean.temperature.deg.C, color='gray') + autolayer(components(dcmp), season_adjust, color='blue') + xlab("Year") + ylab("Temperature (°C)") + ggtitle("Daily Sea Surface Temperature")
+
+
+### Gulf of Mexico data ###
+###########################
+# Read the data 
+gulf_data <- read.csv(here("Project 2", "Data", "Gulf of mexico.csv"), header = TRUE, skip = 6)
+
+# Convert the data frame to a tsibble
+ts_gulf <- gulf_data |>
+  transform(Date = as.Date(paste(year, month, day, sep = "-"))) |>
+  as_tsibble(index = Date)
+
+# Check for missing values
+summary(is.na(ts_gulf$mean.temperature.deg.C)) # <-- no missing values
+
+# Summary statistics
+summary(ts_gulf$mean.temperature.deg.C)
+
+### VISUALISATION ###
+#####################
+
+### Timeseries plot
+
+# Plot the temperature data using ggplot2
+ggplot(ts_gulf, aes(x = Date, y = mean.temperature.deg.C)) +
+  geom_line() +
+  labs(title = "Daily Sea Surface Temperature",
+       x = "Date",
+       y = "Temperature (°C)") +
+  theme_minimal()
+
+# Same with autoplot
+ts_gulf |> autoplot(mean.temperature.deg.C) + ggtitle("Daily Sea Surface Temperature") + ylab("Temperature (°C)") + xlab("Time")
+
+### Seasonal plot
+
+# Plot the seasonality with gg_season
+ts_gulf |>
+  gg_season(mean.temperature.deg.C, labels = "both") +
+  labs(y = "Temperature (°C)",
+       title = "Daily Sea Surface Temperature")
+
+### STL Decomposition ###
+#########################
+
+# STL decomp
+dcmp_gulf <- ts_gulf |> model(STL(mean.temperature.deg.C))
+components(dcmp_gulf)
+
+# Plot the trend
+ts_gulf |> 
+  autoplot(mean.temperature.deg.C, color='gray') + autolayer(components(dcmp_gulf), trend, color='red') + xlab("Year") + ylab("Temperature (°C)") + ggtitle("Daily Sea Surface Temperature")
+
+# Show all compononents of our time serie
+components(dcmp_gulf) %>% autoplot() + xlab("Year")
+
+# Plot season adjusted
+ts_gulf |>
+  autoplot(mean.temperature.deg.C, color='gray') + autolayer(components(dcmp_gulf), season_adjust, color='blue') + xlab("Year") + ylab("Temperature (°C)") + ggtitle("Daily Sea Surface Temperature")
+
+
--- Project 2/Scripts/ETS.R
+++ Project 2/Scripts/ETS.R
@@ -5,36 +5,81 @@ library(here)
 library(tsibble)
 library(ggplot2)
 library(fpp3)
+library(dplyr)
 
 
 ### Atlantic - ETS Forecast ####
-##################################
+################################
 
-#### Data preparation
+#### Data preparation ####
 
-# Read the data 
+# Read the data
 atl_data <- read.csv(here("Project 2", "Data", "Atlantic sea.csv"), header = TRUE, skip = 6)
 
-# Convert the data frame to a tsibble
-ts_atl <- atl_data |>
-  transform(Day = as.Date(paste(year, month, day, sep = "-"))) |>
-  filter(Day >= "2000-01-01") |>
-  as_tsibble(index = Day)
+# Drop mean.temperature.uncertainty, and mean.temperature.kelvin columns
+atl_data <- subset(atl_data, select = -c(mean.temperature.uncertainty, mean.temperature.kelvin, day))
 
-#### ETS Forecast
+# Calculate monthly mean temperature
+atl_data <- atl_data |>
+  group_by(year, month) |>
+  summarise(mean_temp = mean(mean.temperature.deg.C, na.rm = TRUE),
+            .groups = 'drop') |>
+  mutate(month_year = yearmonth(paste(year, month, sep = "-")))
 
-# Fitting the ETS model
-ets_atl_fit <- ts_atl |> model(ETS(mean.temperature.deg.C))
+# Create a tsibble object
+ts_atl <- as_tsibble(atl_data, index = month_year)
 
-atl_fit <- ts_atl |> model(ETS(mean.temperature.deg.C ~ error("A") + trend("A") + season("A")))
+#### ETS Forecast ####
 
-report(atl_fit)
+# Fitting ETS Auto Model : ETS(M, Ad, A)
+ets_atl_fit <- ts_atl |> model(ETS(mean_temp))
+report(ets_atl_fit)
 
 # Plot the forecast for the next N year
-atl_fit |> forecast(h = 365) |> autoplot(ts_atl)
+ets_atl_fit |> forecast(h = 120) |> autoplot(ts_atl)
 
 # Model Accuracy
-atl_fit |> accuracy()
+ets_atl_fit |> accuracy()
 
 # Residuals of the model
-atl_fit |> gg_tsresiduals(lag_max = "3 years")
+ets_atl_fit |> gg_tsresiduals(lag_max = 120)
+
+
+### Gulf of Mexico - ETS Forecast ####
+######################################
+
+
+#### Data preparation ####
+
+# Read the data
+gulf_data <- read.csv(here("Project 2", "Data", "Gulf of mexico.csv"), header = TRUE, skip = 6)
+
+# Drop mean.temperature.uncertainty, and mean.temperature.kelvin columns
+gulf_data <- subset(gulf_data, select = -c(mean.temperature.uncertainty, mean.temperature.kelvin, day))
+
+# Calculate monthly mean temperature
+gulf_data <- gulf_data |>
+  group_by(year, month) |>
+  summarise(mean_temp = mean(mean.temperature.deg.C, na.rm = TRUE),
+            .groups = 'drop') |>
+  mutate(month_year = yearmonth(paste(year, month, sep = "-")))
+
+# Create a tsibble object
+ts_gulf <- as_tsibble(gulf_data, index = month_year)
+
+#### ETS Forecast ####
+
+# Fitting ETS Auto Model : ETS(A, A, A)
+ets_gulf_fit <- ts_gulf |> model(ETS(mean_temp ~ error("A") + trend("A") + season("A")))
+report(ets_gulf_fit)
+
+# Plot the forecast for the next N year
+ets_gulf_fit |> forecast(h = 120) |> autoplot(ts_gulf)
+
+# Model Accuracy
+ets_gulf_fit |> accuracy()
+
+# Residuals of the model
+ets_gulf_fit |> gg_tsresiduals(lag_max = 120)
+
+
